02fdd15e2d46b7ab83b18dea1824fcbe3a64af59
newstar1234 <maori36@naver.com>
2023-09-08 16:26:53 +0900

    23.09.08.15:30

--- src/views/Board/Detail/index.tsx
+++ src/views/Board/Detail/index.tsx
@@ -1,7 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router-dom';
 import { getBoardRequest } from 'src/apis';
 import { MAIN_PATH } from 'src/constants';
+import { GetBoardResponseDto } from 'src/interfaces/response/board';
+import ResponseDto from 'src/interfaces/response/response.dto';
+import { dateFormat } from 'src/utils';
+import defaultProfileImage from 'src/assets/default-profile-icon.png';
+
+import './style.css';
+import { useUserStore } from 'src/stores';
+
 
 
 //              component             //
@@ -11,6 +19,8 @@ export default function BoardDetail() {
     //              state               //
     // description : 게시물 번호 path variable 상태 //
     const { boardNumber } = useParams();
+    // description : 로그인 유저 정보 상태 //
+    const { user } = useUserStore();
 
     //              function              //
     // description : navigate 함수 //
@@ -20,8 +30,35 @@ export default function BoardDetail() {
     // description :  게시물 내용 컴포넌트 //
     const Board = () => {
 
-    //              function              //
+    //              state               //
+    // description : 게시물 상태 //
+    const [board, setBoard] = useState<GetBoardResponseDto | null>(null);
+    // description : 본인 게시물 여부 상태 //
+    const [isWriter, setWriter] = useState<boolean>(false);
+    // description : more 버튼 상태 //
+    const [showMore, setShowMore] = useState<boolean>(false);
     
+
+    //              function              //
+    const getBoardResponseHandler = (responseBody: GetBoardResponseDto | ResponseDto) => {
+        const { code } = responseBody;
+        if(code === 'NB') alert('존재하지 않는 게시물입니다.');
+        if(code === 'VF') alert('잘못된 게시물 번호입니다.');
+        if(code === 'DE') alert('데이터 베이스 에러입니다.');
+        if(code !== 'SU') {
+            navigator(MAIN_PATH);
+            return;
+        }   
+
+    const board = responseBody as GetBoardResponseDto;
+    setBoard(board);
+    }
+
+    //              event handler              //
+    // description : more 버튼 클릭 이벤트 처리 //
+    const onMoreButtonClickHandler = () => {
+        setShowMore(!showMore);
+    }
 
     //              effect              //
     // description : 게시물 번호가 바뀔 때 마다 실행 //
@@ -31,37 +68,48 @@ export default function BoardDetail() {
             navigator(MAIN_PATH);
             return;
         }
-        getBoardRequest(boardNumber)
+        getBoardRequest(boardNumber).then(getBoardResponseHandler);
     }, [boardNumber]);
+    // description : 게시물과 유저 정보가 바뀔 때마다 실행 //
+    useEffect (() => {
+        const isWriter = user?.email === board?.writerEmail;
+        setWriter(isWriter);
+    }, [board, user]);
 
     //              render              //
     return (
         <div className='board-detail-container'>
             <div className='board-detail-header'>
-                <div className='board-detail-title'></div>
+                <div className='board-detail-title'>{board?.title}</div>
                 <div className='board-detail-meta-data'>
                     <div className='board-detail-write-data'>
-                        <div className='board-detail-writer-profile-image'></div>
-                        <div className='board-detail-writer-nickname'></div>
-                        <div className='board-detail-meta-divider'></div>
-                        <div className='board-detail-write-date'></div>
+                        <div className='board-detail-writer-profile-image' style={{backgroundImage: `url(${board?.writerProfileImage ? board.writerProfileImage : defaultProfileImage})`}}></div>
+                        <div className='board-detail-writer-nickname'>{board?.writerNickname}</div>
+                        <div className='board-detail-meta-divider'>{'\|'}</div>
+                        <div className='board-detail-write-date'>{dateFormat(board?.writeDatetime as string)}</div>
                     </div>
                     <div className='board-detail-more-button-box'>
+                        {showMore && (
                         <div className='more-button-group'>
-                            <div className='more-button'></div> 
+                            <div className='more-button'>{'수정'}</div> 
                             <div className='divider'></div> 
-                            <div className='more-button-red'></div> 
-                        </div>
-                        <div className='board-detail-more-button'>
+                            <div className='more-button-red'>{'삭제'}</div> 
+                         </div>
+                        )}
+                        {isWriter && (
+                        <div className='board-detail-more-button' onClick={onMoreButtonClickHandler} >
                             <div className='more-icon'></div>
                         </div>
+                        )}
                     </div>
                 </div>
             </div>
             <div className='divider'></div>
             <div className='board-detail-body'>
-                <div className='board-detail-contents'></div>
-                <div className='board-detail-image-box'></div>
+                <div className='board-detail-contents'>{board?.content}</div>
+                <div className='board-detail-image-box'>
+                    <img className='board-detail-image' src={board?.imageUrl ? board.imageUrl : ''} />
+                </div>
             </div>
         </div>
     );
